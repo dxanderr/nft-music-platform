234964832ec92fd51a3cf923a9ed4e00a51e661c
Don <dxanderr@gmail.com>
2022-03-02 22:18:07 +0100

    first commit

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+export default function Header(){
+    return(
+        <div className="header--container">
+            <div className="logo">
+                <strong>Music</strong>
+                <p>NFT Platform</p>
+            </div>
+ 
+            <nav>
+                <p>MARKETPLACE</p>
+                <p>ABOUT</p>
+                <p>RESOURCES</p>
+                <p>FAQ</p>
+            </nav>
+
+            <div className="social--links">
+
+            </div>
+        </div>
+    )
+}
