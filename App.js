630dd52c986cc2d26fa9ca4a900fc4615f2901d4
Don <dxanderr@gmail.com>
2022-03-02 22:57:25 +0100

    header edits, second blur added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import topLeft from './images/top-left.png'
+import ellipse from './images/ellipse.png'
 
 export default function App(){
     return(        
         <div className="home--container">
             <img src={topLeft} className="top-left-blur" alt="bg-blur"/>
+            <img src={ellipse} className="bottom-right-blur" alt="bg-blur"/>
             <Header />
         </div>
     )
